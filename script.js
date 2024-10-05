ab05672f45ea38ff88f322e446d1a6c7df88cf6d
Sam <samhooker@gmail.com>
2024-10-05 09:18:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// const mobileMenu = document.querySelector('.mobile-menu')
+// const hamburgerMenu = document.querySelector('.hamburger-menu')
+// const before = document.querySelector('.hamburger-menu::before')
+// const mainMenu = document.querySelector('.main-menu')
+
+// function openMobileMenu() {
+// 	console.log('click')
+// 	mainMenu.style.display = 'flex'
+// 	mainMenu.style.opacity = 1
+// 	hamburgerMenu.style.backgroundColor = 'red'
+// }
+
+// mobileMenu.addEventListener('click', openMobileMenu)
